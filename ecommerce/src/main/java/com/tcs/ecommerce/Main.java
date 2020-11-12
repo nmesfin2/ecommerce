9cff884787a4130fc7faedc87c17b9bbc06ee681
nat <nmexodus870@gmail.com>
2020-11-12 15:18:58 +0000

    checked if singlton works

--- ecommerce/src/main/java/com/tcs/ecommerce/Main.java
+++ ecommerce/src/main/java/com/tcs/ecommerce/Main.java
@@ -1,5 +1,7 @@
 package com.tcs.ecommerce;
 
+import com.tcs.ecommerce.dao.ProductDAO;
+import com.tcs.ecommerce.dao.ProductDAOImpl;
 import com.tcs.ecommerce.model.Product;
 
 public class Main {
@@ -7,5 +9,11 @@ public class Main {
 	public static void main(String [] args) {
 		Product product = new Product();
 		
+		ProductDAO dao = ProductDAOImpl.getInstance();
+		ProductDAO dao2 = ProductDAOImpl.getInstance();
+		
+		System.out.println(dao.hashCode());
+		System.out.println(dao2.hashCode());
+		System.out.println(dao.equals(dao2));
 	}
 }
