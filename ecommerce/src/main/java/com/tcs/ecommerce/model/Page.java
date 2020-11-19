635f42dfec06accb05c631d43130d7719aa5016d
nat <nmexodus870@gmail.com>
2020-11-19 15:53:16 +0000

    OneToMany and ManyToOne implemented.

---
+++ ecommerce/src/main/java/com/tcs/ecommerce/model/Page.java
@@ -0,0 +1,32 @@
+package com.tcs.ecommerce.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Page {
+	
+	private long id;
+	private int number;
+	private String content;
+	private String chapter;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="book_id")
+	private Book book;
+	
+}
